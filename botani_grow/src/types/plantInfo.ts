6d85ff184dc81cbe665449c52d4cf2fc1c79fc2c
N <hatarakuotamasan8@outlook.jp>
2023-10-23 23:58:19 +0900

    debug wateinrgsData Type

--- botani_grow/src/types/plantInfo.ts
+++ botani_grow/src/types/plantInfo.ts
@@ -4,13 +4,8 @@ export type PlantInfo = {
   name: string;
   size: string;
   leafCount: number;
-  // wateringCycle: number; // 水やりの頻度(日数)
   tags: string[] | null;
   startDate: Date;
   wateringAmount: string; // 水やりの量(多, ふつう, 少)
   isArchived: boolean;
 };
-
-// export type WateringsInfo = {
-//   nextWateringDate:
-// }
