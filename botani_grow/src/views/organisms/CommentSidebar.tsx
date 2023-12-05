9912e4d0d92cd5551ad80660dd8e3e858c1ad964
N <hatarakuotamasan8@outlook.jp>
2023-12-05 20:34:13 +0900

    update memo sidebar ui

--- botani_grow/src/views/organisms/CommentSidebar.tsx
+++ botani_grow/src/views/organisms/CommentSidebar.tsx
@@ -6,7 +6,7 @@ import { PlantMemo } from '../../types/plantMemo';
 
 import { IconContext } from 'react-icons';
 import { IoIosClose } from 'react-icons/io';
-import { RiDeleteBin6Fill } from 'react-icons/ri';
+import { AiOutlineDelete } from 'react-icons/ai';
 import { TbChevronsRight } from 'react-icons/tb';
 import { HiOutlineArrowSmDown } from 'react-icons/hi';
 import { db } from '../../config/Firebase';
@@ -107,13 +107,12 @@ export const CommentSidebar: React.FC<CommentSidebarProps> = ({
           <button className="close-btn" onClick={toggleCommentSidebar}>
             <TbChevronsRight className="close-icon" />
           </button>
-          <span className="whitespace-nowrap rounded-lg bg-slate-700 px-2 py-1 text-xs text-white absolute top-14 left-0 opacity-0 group-hover:opacity-100 transition pointer-events-none z-10">
+          <span className="whitespace-nowrap rounded-lg bg-slate-700 px-2 py-1 text-xs text-white absolute top-14 right-3 opacity-0 group-hover:opacity-100 transition pointer-events-none z-10">
             Close sidebar
           </span>
         </span>
         <div className="comment-input">
           <label>
-            Memo
             <textarea
               className={isExpanded ? 'expanded' : ''}
               onFocus={() => setIsExpanded(true)}
@@ -121,7 +120,7 @@ export const CommentSidebar: React.FC<CommentSidebarProps> = ({
               name="commentText"
               maxLength={100}
               spellCheck="true"
-              placeholder="write here..."
+              placeholder="write memo here..."
               value={inputText}
               onChange={(e) => setInputText(e.target.value)}
               // onFocus={(e) => e.currentTarget.parentElement?.focus()}
@@ -130,7 +129,7 @@ export const CommentSidebar: React.FC<CommentSidebarProps> = ({
               <button className="" type="button" onMouseDown={handleSubmit}>
                 <HiOutlineArrowSmDown className="text-lg" />
               </button>
-              <span className="whitespace-nowrap rounded-lg bg-slate-700 px-2 py-1 text-xs text-white absolute -top-8 -left-1 opacity-0 group-hover:opacity-100 transition pointer-events-none">
+              <span className="whitespace-nowrap rounded-lg bg-slate-700 px-2 py-1 text-xs text-white absolute -top-7 -left-1 opacity-0 group-hover:opacity-100 transition pointer-events-none">
                 Add
               </span>
             </span>
@@ -138,7 +137,7 @@ export const CommentSidebar: React.FC<CommentSidebarProps> = ({
         </div>
         <div className="past-comments">
           {memos.map((memo, id) => (
-            <div key={id} className="single-comment">
+            <div key={id} className="single-comment hover:bg-gray-100/75">
               <div className="flex justify-between">
                 <span className="text-xs text-slate-400">
                   {memo.date.toDate().toLocaleString('ja-JP', {
@@ -154,7 +153,7 @@ export const CommentSidebar: React.FC<CommentSidebarProps> = ({
                   >
                     {' '}
                     <IconContext.Provider value={{ color: '#4b4a4a' }}>
-                      <RiDeleteBin6Fill className="close-icon" />
+                      <AiOutlineDelete className="close-icon" />
                     </IconContext.Provider>
                   </button>
                   <span className="whitespace-nowrap rounded-lg bg-slate-700 px-2 py-1 text-xs text-white absolute top-4 -left-5 opacity-0 group-hover:opacity-100 transition pointer-events-none">
